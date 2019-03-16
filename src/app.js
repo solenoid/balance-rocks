0a9d2b1e1ad62b8b5cb960857f17a7cd0a96a9d8
Erik Solen <erikshaner@gmail.com>
2019-03-15 21:17:45 -0400

    use css grid and js calculated masonry layout

--- src/app.js
+++ src/app.js
@@ -1,21 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { hot } from "react-hot-loader/root";
 import { CANNED_RESPONSE } from "./canned.js";
 
 const App = () => {
-  const [howMany, setHowMany] = useState(8);
+  const [_, forceUpdate] = useState();
+  const [colWidth, setColWidth] = useState(0);
+  const [howMany, setHowMany] = useState(9);
+  useEffect(() => {
+    // NOTE 20 px and 3 columns should be coordinated with css setup
+    setColWidth(Math.floor((window.innerWidth - 20) / 3));
+  });
+  useEffect(() => {
+    // TODO consider debouncing the resize
+    window.addEventListener("resize", forceUpdate);
+    return () => {
+      window.removeEventListener("resize", forceUpdate);
+    };
+  });
+  // take the off the wire API and prepare it for just what's needed to render
   const photos = CANNED_RESPONSE.photos.slice(0, howMany).map(d => {
     // TODO stop assuming url structure that seems to happen in practice
     const urlParts = d.url.split("/");
     const nameParts = urlParts[urlParts.length - 2].split("-");
     // remove the number part of the name
     nameParts.pop();
+    console.log();
     return {
       url: d.url,
-      src: d.src.tiny, // .original + "?auto=compress&cs=tinysrgb&w=200",
+      scaledHeight: (colWidth * d.height) / d.width,
+      // TODO figure out if we want to have width be hardcoded here or more dynamic
+      src: d.src.original + "?auto=compress&cs=tinysrgb&w=420",
       alt: nameParts.join(" ") + " by " + d.photographer,
     };
   });
+  // organize photos into a masonry layout based on varying heights
+  const [[c1, c2, c3]] = photos.reduce(
+    (memo, cur) => {
+      let [[c1, c2, c3], [h1, h2, h3]] = memo;
+      const csh = 0.8 * cur.scaledHeight;
+      const additionalHeight = cur.scaledHeight + 5;
+      if (h1 < h2 + csh && h1 < h3 + csh) {
+        c1.push(cur);
+        h1 += additionalHeight;
+      } else if (h2 < h3 + csh) {
+        c2.push(cur);
+        h2 += additionalHeight;
+      } else {
+        c3.push(cur);
+        h3 += additionalHeight;
+      }
+      return [[c1, c2, c3], [h1, h2, h3]];
+    },
+    [[[], [], []], [0, 0, 0]],
+  );
+
   return (
     <>
       <h1>Solen Dev Balance Rocks</h1>
@@ -28,11 +66,29 @@ const App = () => {
           &rsaquo;
         </button>
       </div>
-      {photos.map(p => (
-        <a href={p.url} key={p.url} target="photo">
-          <img src={p.src} alt={p.alt} />
-        </a>
-      ))}
+      <div className="masonry">
+        <div id="col-1">
+          {c1.map(p => (
+            <a href={p.url} key={p.url} target="photo">
+              <img src={p.src} alt={p.alt} style={{ width: colWidth }} />
+            </a>
+          ))}
+        </div>
+        <div id="col-2">
+          {c2.map(p => (
+            <a href={p.url} key={p.url} target="photo">
+              <img src={p.src} alt={p.alt} style={{ width: colWidth }} />
+            </a>
+          ))}
+        </div>
+        <div id="col-3">
+          {c3.map(p => (
+            <a href={p.url} key={p.url} target="photo">
+              <img src={p.src} alt={p.alt} style={{ width: colWidth }} />
+            </a>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
